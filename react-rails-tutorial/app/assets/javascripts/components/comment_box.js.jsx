fedd8dd2a063865b038ab1c34a8b62cfd0961928
shu_abe <kaelapiece@gmail.com>
2017-01-19 13:06:56 +0900

    子Componentの定義を追加

--- react-rails-tutorial/app/assets/javascripts/components/comment_box.js.jsx
+++ react-rails-tutorial/app/assets/javascripts/components/comment_box.js.jsx
@@ -2,7 +2,39 @@ var CommentBox = React.createClass({
   render: function() {
     return (
       <div className="commentBox">
-        Hello, world! I am a CommentBox.
+        <h1>Comments</h1>
+        <CommentList />
+        <CommentForm />
+        <ComentTest />
+      </div>
+    );
+  }
+});
+
+var CommentList = React.createClass({
+  render: function() {
+    return (
+      <div className="commentList">
+        Hello, world! I am a CommentList.
+      </div>
+    );
+  }
+});
+
+var CommentForm = React.createClass({
+  render: function() {
+    return (
+      <div className="commentForm">
+        Hello, world! I am a CommentForm.
+      </div>
+    );
+  }
+});
+var ComentTest = React.createClass({
+  render: function() {
+    return (
+      <div className="commentTest">
+        テスト成功！
       </div>
     );
   }
